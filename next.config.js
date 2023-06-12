57448b182c4e9b0dc8316ee5b157dd76caf06206
yukimark <mark2peace@gmail.com>
2023-06-12 14:58:40 +0900

    styled-components周りのインストール、設定

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  // 以下を追加
+  compiler: {
+      styledComponents: true,
+  },
 }
 
 module.exports = nextConfig

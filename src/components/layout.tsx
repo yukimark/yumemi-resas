fe38f0d2d5b5b23a9e320338cfc275c212d4e4ec
yukimark <mark2peace@gmail.com>
2023-06-23 14:59:47 +0900

    トップページのレイアウト変更

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,10 +1,18 @@
 import { Header } from './base/header'
+import styled from 'styled-components'
+
+const ContentsBox = styled.div`
+  width: 1000px;
+  margin: 0 auto;
+`
 
 export function Layout({ children }) {
   return (
     <>
-      <Header />
-      <main>{ children }</main>
+      <ContentsBox>
+        <Header />
+        <main>{ children }</main>
+      </ContentsBox>
     </>
   )
 }
